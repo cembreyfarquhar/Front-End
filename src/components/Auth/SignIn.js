eb184781d633275534302a9efd9c42c39b5cdd97
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-06-24 15:16:01 -0500

    updates gitignore, adds axios to deps

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
-      <h2>Log in or Sign up</h2>
+      <h2>Welcome back. Log in below</h2>
+      <form>
+        <input
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </form>
+      <h3>
+        Don't have an account? Click <button>here</button> to sign up
+      </h3>
     </>
   );
 }
